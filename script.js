385630985561a51f56c2b1ad770a110671a5da87
Naman Ganatra <86153723+NamanGanatra@users.noreply.github.com>
2026-09-04 22:46:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+import { getFirestore, collection, getDocs, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+
+
 // Firebase Configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBE_9UuuWsiIPlxHLIFI0LegMxAzsrYeH0",
